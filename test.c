18c17a8d2dcc596dcb3796f3e4504b8157448baf
minhoryang <minhoryang@gmail.com>
2014-06-06 22:42:34 +0900

    Rename test files.

--- test.c
+++ test.c
@@ -1,21 +1,44 @@
-int num;
-int hi;
-void main(void)
+/* fibonacci */
+int iterate(int n)
 {
-	
+	int a;
+	int b;
+	int c;
 	int i;
-	int j;
-	int k;
-	i=1;
-	j=2;
-	k=addnum(i,j);
-	{
-	/*	int sibal;
-		int compiler;
-		sibal= input();
-		compiler= sibal;
-		output(sibal);*/
+	a = 1;
+	b = 1;
+	if( n == 0 ){
+		return 0;
 	}
-	output(k);
+	i = 3;
+	while(i <= n){
+		int c;
+		c = a + b;
+		a = b;
+		b = c;
+		i = i + 1;
+	}
+	return b;
 }
 
+int recur(int n)
+{
+	if(n == 0)
+		return 0;
+	else if(n == 1)
+		return 1;
+	else if(n > 1)
+		return recur(n-1) + recur(n-2);
+	else
+		return 0;
+}
+int a;
+int b[2];
+void main(void){
+	int x;
+	int y;
+	x = input();
+	output(iterate(x));
+	y = input();
+	output(recur(y));
+}
